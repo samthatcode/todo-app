c626b9424454e2b099aa8448266fbf731a7fc996
samthatcode <samthatcode@gmail.com>
2023-05-17 22:21:55 +0100

    Added the CRUD functionality

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./components/Todo";
+import { db } from "./firebase";
+import {
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#223a45] to-[#2da3b5]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex flex-col sm:flex-row justify-between`,
+  input: `border p-2 w-full text-xl rounded-md mb-2 sm:mb-0`,
+  button: `border p-4 ml-2 bg-gray-700 rounded-md text-slate-100 hover:bg-green-700 focus:bg-green-700 flex justify-center`,
+  count: `text-center text-sm p-2 text-gray-700`,
+};
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  // Create todo
+  const createTodo = async (e) => {
+    e.preventDefault();
+    if (input === "") {
+      alert("Please enter a valid todo");
+      return;
+    }
+    await addDoc(collection(db, "todos"), {
+      text: input,
+      completed: false,
+    });
+    setInput("");
+  };
+
+  // Read todo from firebase
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let todosArr = [];
+      QuerySnapshot.forEach((doc) => {
+        todosArr.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArr);
+    });
+    return () => unsubscribe;
+  }, []);
+
+  // Update todo in firebase
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), {
+      completed: !todo.completed,
+    });
+  };
+
+  // Delete todo
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
+
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form onSubmit={createTodo} className={style.form}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            className={style.input}
+            placeholder="Add Todo"
+          />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo
+              key={index}
+              todo={todo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+            />
+          ))}
+        </ul>
+        {todos.length < 1 ? null : (
+          <p className={style.count}>{`You have ${todos.length} todo(s)`}</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
+
+const style = {
+  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize mt-4 rounded-md`,
+  liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize mt-4 rounded-md`,
+  row: `flex`,
+  text: `ml-2 cursor-pointer`,
+  textComplete: `ml-2 cursor-pointer line-through`,
+  button: `cursor-pointer flex items center hover:text-red-700`,
+};
+
+const Todo = ({ todo, toggleComplete, deleteTodo }) => {
+  return (
+    <li className={todo.completed ? style.liComplete : style.li}>
+      <div className={style.row}>
+        <input
+          onChange={() => toggleComplete(todo)}
+          type="checkbox"
+          checked={todo.completed ? "checked" : ""}
+        />
+        <p
+          onClick={() => toggleComplete(todo)}
+          className={todo.completed ? style.textComplete : style.text}
+        >
+          {todo.text}
+        </p>
+      </div>
+      <button onClick={() => deleteTodo(todo.id)} className={style.button}>
+        <FaRegTrashAlt size={20} />
+      </button>
+    </li>
+  );
+};
+
+export default Todo;
